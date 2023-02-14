82d93050be5e34117a432a19bdf3b3c2e91a5731
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-02-14 16:55:28 -0300

    estilizando a pagina e criando componente grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
+import { GridItem } from './components/GridItem';
+
+import { levels, calculateImc } from './helpers/imc';
 
 function App() {
+  const [heightField, setHeightField] = useState<number>();
+  const [weightField, setWeightField] = useState<number>();
+
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+    } else {
+      alert('Digite todos os campos');
+    }
+  };
+
   return (
     <div>
       <header>
@@ -10,8 +24,36 @@ function App() {
         </div>
       </header>
       <div className={styles.container}>
-        <div className={styles.leftSide}>...</div>
-        <div className={styles.rightSide}>...</div>
+        <div className={styles.leftSide}>
+          <h1>Calcule seu IMC.</h1>
+          <p>
+            IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela
+            Organização Mundial de Saúde para calcular o peso ideal de cada
+            pessoa.
+          </p>
+
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
+            value={heightField}
+            onChange={(e) => setHeightField(parseFloat(e.target.value))}
+          />
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex: 75.3 (em kg)"
+            value={weightField}
+            onChange={(e) => setWeightField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+        </div>
+        <div className={styles.rightSide}>
+          <div className={styles.grid}>
+            {levels.map((item, key) => (
+              <GridItem key={key} item={item} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
