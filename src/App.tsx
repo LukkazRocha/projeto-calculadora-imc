55b85ce52de135e1be28057bd41b3c2d2219a62a
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-02-15 16:48:06 -0300

    deixando os calculos funcionais

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
+import leftArrowImage from './assets/leftArrow.png';
 import { GridItem } from './components/GridItem';
 
-import { levels, calculateImc } from './helpers/imc';
+import { levels, calculateImc, Level } from './helpers/imc';
 
 function App() {
-  const [heightField, setHeightField] = useState<number>();
-  const [weightField, setWeightField] = useState<number>();
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>();
 
   const handleCalculateButton = () => {
     if (heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField));
     } else {
       alert('Digite todos os campos');
     }
+  };
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
   };
 
   return (
@@ -35,24 +44,41 @@ function App() {
           <input
             type="number"
             placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
-            value={heightField}
+            value={heightField > 0 ? heightField : ''}
             onChange={(e) => setHeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
           <input
             type="number"
             placeholder="Digite o seu peso. Ex: 75.3 (em kg)"
-            value={weightField}
+            value={weightField > 0 ? weightField : ''}
             onChange={(e) => setWeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
 
-          <button onClick={handleCalculateButton}>Calcular</button>
+          <button
+            onClick={handleCalculateButton}
+            disabled={toShow ? true : false}
+          >
+            Calcular
+          </button>
         </div>
         <div className={styles.rightSide}>
-          <div className={styles.grid}>
-            {levels.map((item, key) => (
-              <GridItem key={key} item={item} />
-            ))}
-          </div>
+          {!toShow && (
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} item={item} />
+              ))}
+            </div>
+          )}
+          {toShow && (
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25} />
+              </div>
+              <GridItem item={toShow} />
+            </div>
+          )}
         </div>
       </div>
     </div>
